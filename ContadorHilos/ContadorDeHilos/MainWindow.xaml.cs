977d8e402aff058d0041af1db70e269b73a12ae8
gaby2327 <gabriela.uv@outlook.com>
2020-03-23 17:07:06 -0600

    Create ContadorHilos
    Creación de actividad 5

---
+++ ContadorHilos/ContadorDeHilos/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContadorDeHilos
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int contadorManual = 1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonIniciar_Click(object sender, RoutedEventArgs e)
+        {
+            if (checkBoxHilos.IsChecked.Value == true)
+            {
+                Thread hilo = new Thread(ContadorHilo);
+                hilo.Start();
+            }
+            else
+            {
+                if (checkBoxHilos.IsChecked.Value == false)
+                {
+                    ContadorSinHilo();
+                }
+            }
+        }
+
+        private void ContadorHilo()
+        {
+            int cuentaAutomatica = 1;
+            while (cuentaAutomatica <= 100)
+            {
+                textBoxCuentaAutomatica.Dispatcher.Invoke(new Action(() =>
+                {
+                    textBoxCuentaAutomatica.Text = cuentaAutomatica.ToString();
+                }));
+                cuentaAutomatica++;
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void ContadorSinHilo()
+        {
+            int cuentaAutomatica = 1;
+            while (cuentaAutomatica <= 100)
+            {
+                textBoxCuentaAutomatica.Text = cuentaAutomatica.ToString();
+                cuentaAutomatica++;
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void buttonAumentar_Click(object sender, RoutedEventArgs e)
+        {
+            contadorManual++;
+            textBoxAumentoManual.Text = contadorManual.ToString();
+        }
+    }
+}
